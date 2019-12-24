316338cd1b407985158e7096c4a2333dfab2bc16
Kavantek <gleb00099@gmail.com>
2019-12-24 04:22:50 +0300

    Calc

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, f;
+            string d;
+            do
+            {
+                Console.WriteLine("Ready");
+                x = Convert.ToInt32(Console.ReadLine());
+                d = Console.ReadLine();
+                y = Convert.ToInt32(Console.ReadLine());
+                if (d == "+") { Console.WriteLine(x + y); }
+                if (d == "-") { Console.WriteLine(x - y); }
+                if (d == "*") { Console.WriteLine(x * y); }
+                if (d == "/") { Console.WriteLine(x / y); }
+                Console.WriteLine("exit 1 \nagain 2 \n");
+                f = Convert.ToInt32(Console.ReadLine());
+            } while (f != 1);
+        }
+    }
+}
