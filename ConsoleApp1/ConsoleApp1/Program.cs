186170c0e33731cb9c07a24d8646df68eeb5f320
Kavantek <gleb00099@gmail.com>
2019-12-24 04:46:20 +0300

    Mani operations

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,18 +10,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int x, y, f;
-            string d;
+            int x, y,s=0, f;
+            string d,d2=" ";
             do
             {
                 Console.WriteLine("Ready");
                 x = Convert.ToInt32(Console.ReadLine());
                 d = Console.ReadLine();
                 y = Convert.ToInt32(Console.ReadLine());
-                if (d == "+") { Console.WriteLine(x + y); }
-                if (d == "-") { Console.WriteLine(x - y); }
-                if (d == "*") { Console.WriteLine(x * y); }
-                if (d == "/") { Console.WriteLine(x / y); }
+                if (d == "+") { s = x + y; }
+                if (d == "-") { s = x - y; }
+                if (d == "*") { s = x * y; }
+                if (d == "/") { s = x / y; }
+                do
+                {
+                    d2 = Console.ReadLine();
+                    if (d2 == "+")
+                    {
+                        x = s;
+                        y = Convert.ToInt32(Console.ReadLine());
+                        s = x + y;
+                    }
+                    if (d2 == "-")
+                    {
+                        x = s;
+                        y = Convert.ToInt32(Console.ReadLine());
+                        s = x - y;
+                    }
+                    if (d2 == "*")
+                    {
+                        x = s;
+                        y = Convert.ToInt32(Console.ReadLine());
+                        s = x * y;
+                    }
+                    if (d2 == "/")
+                    {
+                        x = s;
+                        y = Convert.ToInt32(Console.ReadLine());
+                        s = x / y;
+                    }
+                } while (d2 != "=");
+                Console.WriteLine(s+"\n");
                 Console.WriteLine("exit 1 \nagain 2 \n");
                 f = Convert.ToInt32(Console.ReadLine());
             } while (f != 1);
